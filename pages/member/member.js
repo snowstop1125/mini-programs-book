77e33067880635db767f07b2fdf439c835b78753
snowstop <wangxueting@ganhao.com.cn>
2018-04-13 16:02:06 +0800

    /

--- pages/member/member.js
+++ pages/member/member.js
@@ -8,6 +8,26 @@ Page({
    */
   data: {
     isIpx: app.globalData.isIpx ? true : false,
+    userInfo:{
+      userPic: null,
+      userName: '小书虫'
+    },
+  },
+  chooseImage:function(){
+    var _this=this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var that = _this;
+        var tempFilePaths = res.tempFilePaths
+        that.setData({
+          "userInfo.userPic": tempFilePaths
+        });
+      }
+    })
   },
 
   /**
